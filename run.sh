681c988a1f6bb2cf435ca2d1007cbe33b60e4a25
Thibaud EHRET <thibaud.ehret@ens-cachan.fr>
2022-11-22 19:02:12 +0100

    +

---
+++ run.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+set -e
+
+# Parameters
+gs=$1
+ps=$2
+auto=$3
+tau=$4
+eps=$5
+
+# Do the forgery detection
+if [ "$auto" = True ]; then
+    main -im input_0.png -ps $ps -gs $gs -auto true -tau $eps
+else
+    main -im input_0.png -ps $ps -gs $gs -auto false -tau $tau
+fi;
+
+# Check if something has been detected
+matches=`wc -l < data_matches.csv`
+
+# Produce the final results
+if [ "$matches" -gt 1 ]; then
+    echo "isforgery=1" > algo_info.txt
+    echo "This image IS a forgery" > result.txt
+else #[ "$Type" == 2 ]; then
+    echo "This image IS NOT a forgery" > result.txt
+fi;
